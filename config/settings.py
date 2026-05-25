da9b7ea4586b77eb9a730ad828e2028630a12a95
Marcin <102833391+marc7nek@users.noreply.github.com>
2026-05-25 19:48:31 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 @dataclass(frozen=True)
 class Settings:
     base_url: str = os.getenv("BASE_URL", "https://api.github.com")
-    api_version: str = os.getenv("GITHUB_API_VERSION", "2022-11-28")
+    api_version: str = os.getenv("GITHUB_API_VERSION", "2026-03-10")
     github_token: str = os.getenv("GITHUB_TOKEN", "")
     demo_username: str = os.getenv("DEMO_USERNAME", "octocat")
     demo_owner: str = os.getenv("DEMO_OWNER", "octocat")
